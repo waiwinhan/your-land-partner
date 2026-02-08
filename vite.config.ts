b8e40496481ae816f8c9b178208a0f82f111443b
Wai Win Han <winhanwai@gmail.com>
2026-02-08 19:06:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
+  base: 'your-land-partner',
+})
+
   server: {
     host: "::",
     port: 8080,
